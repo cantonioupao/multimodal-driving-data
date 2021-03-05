574aed07d86f83724bd5b6d1bc2a49777ff2fbf8
cantonioupao <christos0598pao13@gmail.com>
2021-03-05 22:34:12 +0200

    Checking Data Loading

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from load_data import load_data
+import os
 
 print("So let's start the task")
-load_data('//data')
+data_path = os.path.join("./data", "data.p")
+data = load_data(data_path)
+velodyne =  data['velodyne']
+print(velodyne.shape)
